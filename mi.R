4e1502ea30d2fa8b8cdfd59f693040f9509ebb38
Rob Johnson <rob.johnson@mrc-bsu.cam.ac.uk>
2020-02-26 15:38:02 +0000

    compute mi

---
+++ mi.R
@@ -0,0 +1,206 @@
+source('set_up_script.R')
+
+nIter <- 10000
+draws <- 10000
+cores <<- 16
+registerDoParallel(cores=cores)
+
+highrisk_scalar_samples <<- runif(draws,0,1)
+neighbour_scalar_samples <<- runif(draws,0,1)
+
+number_sampled <- 100#sample(range_informative_clusters,1)
+
+## type 1 error ############################################################
+direct_VE <<- 0
+netwk_list <- list()
+
+neighbour_scalar <<- 0.39
+highrisk_scalar <<- 2.17
+
+for(iter in 1:nIter){
+  ## select random person to start
+  first_infected <- sample(g_name,1)
+  inf_period <- rgamma(length(first_infected),shape=infperiod_shape,rate=infperiod_rate)
+  #hosp_time <- rgamma(length(first_infected),shape=hosp_shape_index,rate=hosp_rate_index)
+  hosp_time <- rtruncnorm(length(first_infected),a=0,mean=hosp_mean_index,sd=hosp_sd_index)
+  inf_time <- min(inf_period,hosp_time)
+  netwk <- simulate_contact_network(neighbour_scalar,high_risk_scalar,first_infected,inf_time,start_day=iter,from_source=0,cluster_flag=0)
+  netwk_list[[iter]] <- netwk
+  
+}
+
+trial_summary <- lapply(netwk_list,summarise_trial,ve_est_temp=0)
+tte <- do.call(rbind,lapply(1:length(trial_summary),function(cluster)if(!is.null(trial_summary[[cluster]]))cbind(trial_summary[[cluster]],cluster)))
+trial_summary <- c()
+ntwks <- unique(tte$cluster)
+
+#profvis({
+par_results <- do.call(rbind,mclapply(1:draws,function(cl){
+  
+  clusters_sampled <- sample(ntwks,number_sampled,replace=F)
+  
+  neighbour_scalar <<- qbeta(neighbour_scalar_samples[cl],5,5)
+  high_risk_scalar <<- 1/qbeta(highrisk_scalar_samples[cl],5,5)
+  
+  pv <- iterate_ph_model(netwk_list[clusters_sampled])
+  trial_summary <- list()
+  #for(i in 1:length(clusters_sampled)) trial_summary[[i]] <- summarise_trial(netwk_list[[clusters_sampled[i]]],ve_est_temp=pv[2])
+  #netwk_list <- c()
+  for(cluster in 1:length(clusters_sampled)) {
+    trial_summary[[cluster]] <- summarise_trial(netwk_list[[clusters_sampled[cluster]]],ve_est_temp=pv[2])
+    if(!is.null(trial_summary[[cluster]]))
+      trial_summary[[cluster]] <- cbind(trial_summary[[cluster]],cluster)
+  }
+  
+  tte <- do.call(bind_rows,trial_summary)
+  #tte <- do.call(rbind,lapply(1:length(trial_summary),function(cluster)if(!is.null(trial_summary[[cluster]]))cbind(trial_summary[[cluster]],cluster)))
+  trial_summary <- c()
+  ttemat <- as.matrix(tte)
+  
+  survmodel <- coxph(Surv(time,outcome)~vaccinated,weights=weight,tte)
+  vaccEffEst <- 1-exp(survmodel$coefficient + c(0, 1.96, -1.96)*as.vector(sqrt(survmodel$var)))
+  zval <- survmodel$coefficient/sqrt(survmodel$var)
+  pval <- pnorm(zval, lower.tail = vaccEffEst[1]>0)*2
+  return(c(pval,sum(ttemat[,7]==T),nrow(ttemat),vaccEffEst )) ## output weights and exposures (time)
+},mc.cores=cores))
+#})
+#netwk_list <- c()
+
+saveRDS(list(par_results,highrisk_scalar_samples,neighbour_scalar_samples),'mit1e.Rds')
+
+
+
+t1e <- par_results[,1]
+VEt1e <- par_results[,4]
+
+outcomes <- list(par_results[,1],par_results[,4])
+variables <- list(highrisk_scalar_samples,neighbour_scalar_samples)
+evppi <- sapply(variables,function(x)
+  sapply(outcomes,
+         function(y)mutinformation(discretize(x),discretize(y))
+  )
+)
+colnames(evppi) <- c('high risk','neighbour')
+rownames(evppi) <- c('power','vacc eff')
+print(evppi)
+
+
+## power ############################################################
+direct_VE <<- 0.8
+netwk_list <- list()
+
+neighbour_scalar <<- 0.39
+high_risk_scalar <<- 2.17
+
+for(iter in 1:nIter){
+  ## select random person to start
+  first_infected <- sample(g_name,1)
+  inf_period <- rgamma(length(first_infected),shape=infperiod_shape,rate=infperiod_rate)
+  #hosp_time <- rgamma(length(first_infected),shape=hosp_shape_index,rate=hosp_rate_index)
+  hosp_time <- rtruncnorm(length(first_infected),a=0,mean=hosp_mean_index,sd=hosp_sd_index)
+  inf_time <- min(inf_period,hosp_time)
+  netwk <- simulate_contact_network(neighbour_scalar,high_risk_scalar,first_infected,inf_time,start_day=iter,from_source=0,cluster_flag=0,direct_VE = direct_VE)
+  netwk_list[[iter]] <- netwk
+  
+}
+
+trial_summary <- lapply(netwk_list,summarise_trial,ve_est_temp=0.8)
+tte <- do.call(rbind,lapply(1:length(trial_summary),function(cluster)if(!is.null(trial_summary[[cluster]]))cbind(trial_summary[[cluster]],cluster)))
+trial_summary <- c()
+ntwks <- unique(tte$cluster)
+
+
+#profvis({
+par_results <- do.call(rbind,mclapply(1:draws,function(cl){
+  
+  clusters_sampled <- sample(ntwks,number_sampled,replace=F)
+  neighbour_scalar <<- qbeta(neighbour_scalar_samples[cl],5,5)
+  high_risk_scalar <<- 1/qbeta(highrisk_scalar_samples[cl],5,5)
+  
+  pv <- iterate_ph_model(netwk_list[clusters_sampled])
+  trial_summary <- list()
+  #for(i in 1:length(clusters_sampled)) trial_summary[[i]] <- summarise_trial(netwk_list[[clusters_sampled[i]]],ve_est_temp=pv[2])
+  #netwk_list <- c()
+  for(cluster in 1:length(clusters_sampled)) {
+    trial_summary[[cluster]] <- summarise_trial(netwk_list[[clusters_sampled[cluster]]],ve_est_temp=pv[2])
+    if(!is.null(trial_summary[[cluster]]))
+      trial_summary[[cluster]] <- cbind(trial_summary[[cluster]],cluster)
+  }
+  
+  tte <- do.call(bind_rows,trial_summary)
+  #tte <- do.call(rbind,lapply(1:length(trial_summary),function(cluster)if(!is.null(trial_summary[[cluster]]))cbind(trial_summary[[cluster]],cluster)))
+  trial_summary <- c()
+  ttemat <- as.matrix(tte)
+  
+  survmodel <- coxph(Surv(time,outcome)~vaccinated,weights=weight,tte)
+  vaccEffEst <- 1-exp(survmodel$coefficient + c(0, 1.96, -1.96)*as.vector(sqrt(survmodel$var)))
+  zval <- survmodel$coefficient/sqrt(survmodel$var)
+  pval <- pnorm(zval, lower.tail = vaccEffEst[1]>0)*2
+  return(c(pval,sum(ttemat[,7]==T),nrow(ttemat),vaccEffEst )) ## output weights and exposures (time)
+},mc.cores=cores))
+#})
+#netwk_list <- c()
+
+saveRDS(list(par_results,highrisk_scalar_samples,neighbour_scalar_samples),'mipower.Rds')
+
+
+
+
+power <- par_results[,1]
+VE <- par_results[,4]
+outcomes3 <- list(t1e,VEt1e,power,VE)
+
+evppi3 <- sapply(variables,function(x)
+  sapply(outcomes3,
+         function(y)mutinformation(infotheo::discretize(x),infotheo::discretize(y))/infotheo::entropy(infotheo::discretize(y))
+  )
+)
+colnames(evppi3) <- c('High-risk scalar','Neighbour scalar')
+rownames(evppi3) <- c('p value (VE=0)','VE estimate (VE=0)','p value (VE=0.8)','VE estimate (VE=0.8)')
+print(evppi3)
+
+sapply(1:4,function(i)infotheo::entropy(infotheo::discretize(outcomes3[[i]])))
+
+#1/(c(var(old_par_results[,1]),var(old_par_results[,4]))/c(var(par_results[,1]),var(par_results[,4])))
+
+{pdf('scalarmi.pdf',height=6,width=2+length(outcomes3)); 
+#    {x11(height=6,width=2+length(outcomes3)); 
+  par(mar=c(10,12,3.5,10))
+      outcome_labels <- rownames(evppi3)
+  labs <- rev(colnames(evppi3))
+  get.pal=colorRampPalette(brewer.pal(9,"Reds"))
+  redCol=rev(get.pal(12))
+  bkT <- seq(max(evppi3)+1e-10, 0,length=13)
+  cex.lab <- 1.5
+  maxval <- round(bkT[1],digits=1)
+  col.labels<- c(0,maxval/2,maxval)
+  cellcolors <- vector()
+  for(ii in 1:length(unlist(evppi3)))
+    cellcolors[ii] <- redCol[tail(which(unlist(evppi3[ii])<bkT),n=1)]
+  color2D.matplot(evppi3,cellcolors=cellcolors,main="",xlab="",ylab="",cex.lab=2,axes=F,border='white')
+  fullaxis(side=2,las=2,at=0:(length(outcomes3)-1)+1/2,labels=rev(outcome_labels),line=NA,pos=NA,outer=FALSE,font=NA,lwd=0,cex.axis=1.2)
+  fullaxis(side=1,las=2,at=(length(labs)-1):0+0.5,labels=labs,line=NA,pos=NA,outer=FALSE,font=NA,lwd=0,cex.axis=1.2)
+  mtext(3,text='Mutual information',line=1,cex=1.3)
+  color.legend(length(labs)+0.5,0,length(labs)+0.8,length(outcomes3),col.labels,rev(redCol),gradient="y",cex=1.2,align="rb")
+  for(i in seq(0,length(outcomes3),by=1)) abline(h=i)
+  for(i in seq(0,length(labs),by=1)) abline(v=i)
+  dev.off()
+}
+
+
+{pdf('outcomedensities.pdf'); par(mfrow=c(2,2),mar=c(3,5,5,2))
+for(i in 1:length(outcomes3)) plot(density(outcomes3[[i]]),frame=F,lwd=2,xlab='',ylab='Density',main=rownames(evppi3)[i],cex.lab=1.5,cex.axis=1.5)
+dev.off()
+}
+
+dis_fun <- function (x, numBins, r = range(x), b = NULL){
+  if(is.null(b)) b = seq(from = r[1], to = r[2], length.out = numBins + 1)
+  y = table(cut(x, breaks = b, include.lowest = TRUE))
+  return(list(y,b))
+}
+
+#old_power <- old_par_results[,1]
+#numBins <- length(unlist(unique(infotheo::discretize(old_power))))
+#bins <- dis_fun(old_power,numBins=numBins)[[2]]
+#entropy(dis_fun(old_power,numBins=numBins,b=bins)[[1]])
+#entropy(dis_fun(power,numBins=numBins,b=bins)[[1]])
---
+++ plotting_functions.R
@@ -0,0 +1,63 @@
+equal_freq_disc <- function(x,bins=NULL,nbins = NROW(x)^(1/3)){
+  if(is.null(bins)){
+    sorted_x <- sort(x)
+    bins <- quantile(x,seq(0,1,length=nbins+1))
+    bins[1] <- bins[1] - 1e-6
+    bins[nbins] <- bins[nbins] + 1e-6
+  }
+  cut_x <- cut(x,bins,labels=1:nbins)
+  return(list(cut_x,bins))
+}
+
+plot_rect <- function(grid_pval,cellcolors=cellcolors,x_breaks,y_breaks,x_points,y_points){
+  par(mfrow=c(1,1),mar=c(c(6,6,2,6)))
+  plot(0,0,col='white',xlim=range(x_breaks),ylim=range(y_breaks),axes=F,xlab='',ylab='')
+  for(i in 1:x_points){
+    for(j in 1:y_points){
+      rect(xleft=x_breaks[i], ybottom=y_breaks[j], xright=x_breaks[i+1], ytop=y_breaks[j+1], density = NULL, angle = 45,
+           col = matrix(cellcolors,nrow=x_points,ncol=y_points,byrow = T)[i,j], border = NULL)
+    }
+  }
+}
+
+
+xs <- rnorm(1000)
+ys <- rlnorm(1000)
+pvals <- abs(ys - xs)
+binx <- equal_freq_disc(xs)
+x_binned <- binx[[1]]
+x_breaks <- binx[[2]]
+biny <- equal_freq_disc(ys)
+y_binned <- biny[[1]]
+y_breaks <- biny[[2]]
+x_points <- length(x_breaks)-1
+y_points <- length(y_breaks)-1
+grid_pval <- matrix(NA,nrow=x_points,ncol=y_points)
+for(i in 1:x_points){
+  for(j in 1:y_points){
+    grid_pvals <- pvals[x_binned==i&y_binned==j]
+    if(length(grid_pvals)>0){
+      grid_pval[i,j] <- sum(grid_pvals<0.5)/length(grid_pvals)
+    }
+  }
+}
+
+#grid_pval <- grid_pval[nrow(grid_pval):1,]
+get.pal=colorRampPalette(brewer.pal(9,"Spectral"))
+redCol=rev(get.pal(10))
+bkT <- seq(max(grid_pval,na.rm=T)+1e-10, min(grid_pval,na.rm=T)-1e-10,length=length(redCol)+1)
+cex.lab <- 1.5
+maxval <- round(bkT[1],digits=1)
+col.labels<- c(0,maxval/2,maxval)
+cellcolors <- vector()
+for(ii in 1:length(unlist(grid_pval)))
+  if(!is.na(grid_pval[ii]))
+    cellcolors[ii] <- redCol[tail(which(unlist(grid_pval[ii])<bkT),n=1)]
+#pdf(paste0('ph',type,metric,'.pdf')); par(mar=c(6,6,2,6))
+#color2D.matplot(grid_pval,cellcolors=cellcolors,x_breaks,y_breaks)
+plot_rect(grid_pval,cellcolors=cellcolors,x_breaks,y_breaks,x_points,y_points)
+fullaxis(side=2,las=1,at=y_breaks,labels=round(y_breaks),line=NA,pos=NA,outer=FALSE,font=NA,lwd=0,cex.axis=1)
+fullaxis(side=1,las=2,at=x_breaks,labels=round(x_breaks),line=NA,pos=NA,outer=FALSE,font=NA,lwd=0,cex.axis=0.8)
+color.legend(max(x_breaks)+0.5,0,max(x_breaks)+1,max(y_breaks),col.labels,rev(redCol),gradient="y",cex=1,align="rb")
+#dev.off()
+
