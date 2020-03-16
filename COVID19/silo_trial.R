c75bfdabf1233baf5f3531e55a7ec531ec32c708
Rob Johnson <rob.johnson@mrc-bsu.cam.ac.uk>
2020-03-16 14:15:01 +0000

    wip

--- COVID19/silo_trial.R
+++ COVID19/silo_trial.R
@@ -15,7 +15,7 @@ trial_designs$weight[(nCombAdapt+1):(nComb*(length(adaptations)+1))] <- 'binary'
 trial_designs$powertst <- trial_designs$VE_esttst <- trial_designs$VE_sdtst <- 
   trial_designs$power <- trial_designs$VE_est <- trial_designs$VE_sd <- trial_designs$vaccinated <- trial_designs$infectious <- trial_designs$enrolled <- 0
 ref_recruit_day <- 30
-registerDoParallel(cores=2)
+registerDoParallel(cores=12)
 func <- get_efficacious_probabilities
 eval_day <- 20
 latest_infector_time <- eval_day - 0
