f3585355d43db9dd13b2b5fe78029886ee70c92e
Rob Johnson <rob.johnson@mrc-bsu.cam.ac.uk>
2020-04-10 14:00:50 +0100

    wip

--- COVID19/sspower.R
+++ COVID19/sspower.R
@@ -79,7 +79,7 @@ for(eval_day in eval_days){
     if(!file.exists(filename)){
       power <- vax <- ss <- rep(0,nCombAdapt)
       for(des in 1:5){
-        res <- readRDS(paste0('storage/res',des,'.Rds'))
+        res <- readRDS(paste0('storage/res',eval_day,des,'.Rds'))
         enrolled_count <- vaccinated_count <- 0
         result_mat <- matrix(0,nrow=nTrials,ncol=4)
         for(tr in 1:nTrials){
