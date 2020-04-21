c2cd06e5ee587ce6db51c6b01897e6fe3db11af2
Rob Johnson <rob.johnson@mrc-bsu.cam.ac.uk>
2020-04-21 15:03:35 +0100

    count by cases

--- COVID19/early_stopping_run.R
+++ COVID19/early_stopping_run.R
@@ -6,6 +6,30 @@ draws <<- 1000
 cores <<- 32
 registerDoParallel(cores=cores)
 
+grid_plot <- function(mat){
+  
+  grid_pval <- mat[nrow(mat):1,]
+  get.pal=colorRampPalette(brewer.pal(9,"Spectral"))
+  redCol=rev(get.pal(10))
+  bkT <- seq(max(grid_pval,na.rm=T)+1e-10, 0-1e-10,length=length(redCol)+1)
+  cex.lab <- 1.5
+  maxval <- round(bkT[1],digits=1)
+  col.labels<- c(0,maxval/2,maxval)
+  cellcolors <- vector()
+  for(ii in 1:length(unlist(grid_pval)))
+    if(!is.na(grid_pval[ii]))
+      cellcolors[ii] <- redCol[tail(which(unlist(grid_pval[ii])<bkT),n=1)]
+  pdf(paste0('figures/es',type,'.pdf')); par(mar=c(6,6,2,6))
+  color2D.matplot(grid_pval,cellcolors=cellcolors,main="",xlab=paste0("Total weight"),ylab=paste0("Case weight"),cex.lab=1,axes=F,border=NA)
+  fullaxis(side=2,las=1,at=0:nrow(grid_pval),labels=round(y_labs),line=NA,pos=NA,outer=FALSE,font=NA,lwd=0,cex.axis=1)
+  fullaxis(side=1,las=2,at=0:ncol(grid_pval),labels=round(x_labs),line=NA,pos=NA,outer=FALSE,font=NA,lwd=0,cex.axis=0.8)
+  color.legend(ncol(grid_pval)+0.5,0,ncol(grid_pval)+1,nrow(grid_pval),col.labels,rev(redCol),gradient="y",cex=1,align="rb")
+  dev.off()
+  
+  
+}
+
+
 compute_grid <- function(type){
   
   results_list <- list()
@@ -23,16 +47,17 @@ compute_grid <- function(type){
   #profvis({
   par_results <- do.call(rbind,mclapply(1:draws,function(cl){
     #number_sampled <- sample(range_informative_clusters,1)
-    clusters_sampled <- sample(1:nIter,150,replace=F)
+    clusters_sampled <- sample(1:nIter,300,replace=F)
+    unlisted <- do.call(rbind,lapply(1:length(clusters_sampled),function(x)cbind(results_list[[clusters_sampled[x]]],x)))
+    up_to <- which(unlisted$inTrial)[ceiling(first_threshold)]
     
     cumulative_participants <- trial_participants[clusters_sampled]
     
-    up_to <- which(cumsum(cumulative_participants)>first_threshold)[1]
     case_weight <- 0
     trial_participants2 <- trial_participants[clusters_sampled[1:up_to]]
     results <- results_list[clusters_sampled[1:up_to]]
     vaccinees2 <- vaccinees[clusters_sampled[1:up_to]]
-    while(case_weight<15){
+    while(case_weight<first_threshold){
       eval_list <- get_efficacious_probabilities(results,vaccinees2,trial_participants2,contact_network=-1)
       case_weight <- sum(eval_list[[3]])
       pval  <- calculate_pval(eval_list[[3]],eval_list[[2]])
@@ -40,16 +65,19 @@ compute_grid <- function(type){
       vaccinees2 <- vaccinees[clusters_sampled[1:(length(results)+2)]]
       results <- results_list[clusters_sampled[1:(length(results)+2)]]
     }
+    tp <- sum(trial_participants2)
     
-    up_to <- which(cumsum(cumulative_participants)>second_threshold)[1]
-    if(is.na(up_to)) up_to <- length(cumulative_participants)
-    trial_participants2 <- trial_participants[clusters_sampled[1:up_to]]
-    results <- results_list[clusters_sampled[1:up_to]]
-    vaccinees2 <- vaccinees[clusters_sampled[1:up_to]]
-    eval_list2 <- get_efficacious_probabilities(results,vaccinees2,trial_participants2,contact_network=-1)
-    pval2  <- calculate_pval(eval_list2[[3]],eval_list2[[2]])
+    up_to <- which(unlisted$inTrial)[ceiling(second_threshold)]
+    while(case_weight<second_threshold){
+      eval_list2 <- get_efficacious_probabilities(results,vaccinees2,trial_participants2,contact_network=-1)
+      pval2  <- calculate_pval(eval_list2[[3]],eval_list2[[2]])
+      case_weight <- sum(eval_list2[[3]])
+      trial_participants2 <- trial_participants[clusters_sampled[1:(length(results)+2)]]
+      vaccinees2 <- vaccinees[clusters_sampled[1:(length(results)+2)]]
+      results <- results_list[clusters_sampled[1:(length(results)+2)]]
+    }
     
-    return(c(pval,sum(eval_list[[3]]),sum(eval_list[[2]]),pval2,sum(eval_list2[[3]]),sum(eval_list2[[2]]))) ## output weights 
+    return(c(pval,sum(eval_list[[3]]),sum(eval_list[[2]]),pval2,sum(eval_list2[[3]]),sum(eval_list2[[2]]),tp,sum(trial_participants2))) ## output weights 
   },mc.cores=cores))
   #})
   results_list <- c()
@@ -57,18 +85,18 @@ compute_grid <- function(type){
   #colnames(par_results) <- c('pval','case_weight','weight')
   #saveRDS(par_results,paste0('storage/',type,'_results.Rds'))
 }
-results <- c()
 
 
-first_thresholds <- seq(800,1200,by=100)
-second_thresholds <- seq(1800,2200,by=100)
+first_thresholds <- seq(18,22,by=2)
+second_thresholds <- seq(30,45,by=5)
 
 ## power ############################################################
 direct_VE <<- 0.7
 type <- 'power'
-powers <- halfways <- matrix(0,nrow=length(first_thresholds),ncol=length(second_thresholds))
-total_interations <- 1000
-for(ti in 1:total_interations){
+powers <- halfways <- ss <- matrix(0,nrow=length(first_thresholds),ncol=length(second_thresholds))
+total_iterations <- 1000
+results <- c()
+for(ti in 1:total_iterations){
   for(i in 1:length(first_thresholds)){
     first_threshold <<- first_thresholds[i]
     for(j in 1:length(second_thresholds)){
@@ -77,17 +105,43 @@ for(ti in 1:total_interations){
       fst <- sum(res2[,1]<0.03)
       snd <- sum(res2[,4]<0.02)
       total <- sum(res2[,1]<0.03|res2[,1]>0.03&res2[,4]<0.02)
-      halfways[i,j] <- halfways[i,j] + fst/draws/total_interations
-      powers[i,j] <- powers[i,j] + total/draws/total_interations
-      results <- rbind(results,c(first_threshold,second_threshold,fst,total))
+      halfways[i,j] <- halfways[i,j] + fst/draws/total_iterations
+      powers[i,j] <- powers[i,j] + total/draws/total_iterations
+      sample_size <- res2[,6]
+      sample_size[res2[,1]<0.03] <- res2[res2[,1]<0.03,3]
+      ss[i,j] <- sum(sample_size)/draws/total_iterations
+      results <- rbind(results,res2)
     }
   }
   print(c(ti))
 }
 print(halfways)
 print(powers)
+print(ss)
 
-saveRDS(list(halfways,powers),'storage/run_es.Rds')
+saveRDS(list(halfways,powers,ss),'storage/run_es.Rds')
+
+resultsdf <- as.data.frame(results)
+resultsdf$interim <- resultsdf$V3<1300
+resultsdf13 <- subset(resultsdf,interim)
+upperbounds <- c(first_thresholds,1300)
+upperbounds[1] <- min(resultsdf13$V3)
+sapply(2:length(upperbounds),function(x){
+  subtab <- subset(resultsdf13,V3<upperbounds[x]&V3>upperbounds[x-1])
+  sum(subtab$V1<0.03)/nrow(subtab)
+})
+bounds <- c(14,25,35,45,55)
+sapply(2:length(bounds),function(x){
+  subtab <- subset(resultsdf13,V2<bounds[x]&V2>bounds[x-1])
+  sum(subtab$V1<0.03)/nrow(subtab)
+})
+
+upperbounds2 <- c(second_thresholds,2300)
+upperbounds2[1] <- min(resultsdf$V6)
+sapply(2:length(upperbounds),function(x){
+  subtab <- subset(resultsdf,V6<upperbounds2[x]&V6>upperbounds2[x-1])
+  sum(subtab$V4<0.02)/nrow(subtab)
+})
 
 ## type 1 error ############################################################
 direct_VE <<- 0
